ad9fc5fa321eb2f41e3cf6a464447c130c22fd2d
john-l-hansen <john.l.hansen@gmail.com>
2020-11-30 20:55:19 -0800

    update general styles and basic content

--- src/pages/404.js
+++ src/pages/404.js
@@ -16,8 +16,8 @@ const NotFound = () => (
           fontSize: "128px",
           color: "var(--primary-color)"
         }}/>
-        <h1>Oops we did not expect that to happen</h1>
-        <p>Have you wondered into the unknow. Let us help you, Please take a look at below options</p>
+        <h1>Uh oh! This wasn't supposed to happen.</h1>
+        <p>You've wondered into the unknown! Let's get back on track, please take a look at below options</p>
       </header>
       <Link to="/" className="button"><RiArrowLeftSLine className="icon -left"/>Back to Homepage</Link>
       <Link to="/contact" className="button -outline">Report this <RiBugLine className="icon -right"/></Link>
